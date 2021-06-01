69390c92a2d2fe5aa1018560e8c1f09f6e090046
Le Nam <namlgcd191254@fpt.edu.vn>
2021-06-01 16:13:17 +0700

    three

--- web/config.php
+++ web/config.php
@@ -1,6 +1,9 @@
 <?php
-$conn =  mysqli_connect('ec2-23-23-128-222.compute-1.amazonaws.com
-', 'njieykiufenpog', '5a3641503e4df1a5f246634be2c25c21dfe91cfe39505b63e4d1f03d58d91809', 'd2ercdkgvftbu4');
-mysqli_set_charset($conn,"utf8");
+$conn_string = "host=ec2-23-23-128-222.compute-1.amazonaws.com
+"
+. " port=5432 dbname=d2ercdkgvftbu4"
+. " user=njieykiufenpog"
+. " password=5a3641503e4df1a5f246634be2c25c21dfe91cfe39505b63e4d1f03d58d91809";
+$conn = pg_connect($conn_string);
 ?>
 

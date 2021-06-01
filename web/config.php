566835e632c65530a97c25ddf85ef4532a455f79
Le Nam <namlgcd191254@fpt.edu.vn>
2021-06-01 17:25:30 +0700

    5655654o

--- web/config.php
+++ web/config.php
@@ -4,6 +4,6 @@ $conn_string = "host=ec2-23-23-128-222.compute-1.amazonaws.com
 . " port=5432 dbname=d2ercdkgvftbu4"
 . " user=njieykiufenpog"
 . " password=5a3641503e4df1a5f246634be2c25c21dfe91cfe39505b63e4d1f03d58d91809";
-$conn = pg_connect($conn_string);
+$dbconn = pg_connect($conn_string);
 ?>
 

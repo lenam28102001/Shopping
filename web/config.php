6923f10d539edcc6b98c4a1ff8e6711e9f9b3d12
Le Nam <namlgcd191254@fpt.edu.vn>
2021-06-01 16:01:30 +0700

    second

--- web/config.php
+++ web/config.php
@@ -1,5 +1,6 @@
 <?php
-$conn =  mysqli_connect('localhost', 'root', '', 'shopofnam');
+$conn =  mysqli_connect('ec2-23-23-128-222.compute-1.amazonaws.com
+', 'njieykiufenpog', '5a3641503e4df1a5f246634be2c25c21dfe91cfe39505b63e4d1f03d58d91809', 'd2ercdkgvftbu4');
 mysqli_set_charset($conn,"utf8");
 ?>
 

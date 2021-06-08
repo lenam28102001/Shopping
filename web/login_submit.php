8701fd58b8dbc000ac1923e021f8b4bbeb0ef320
Le Nam <namlgcd191254@fpt.edu.vn>
2021-06-08 15:24:02 +0700

    354442

--- web/login_submit.php
+++ web/login_submit.php
@@ -9,6 +9,7 @@
             $password = md5($password);
             $sql = "select * from public.user where username = '".pg_escape_string($username)."' and password = '".pg_escape_string($password)."'";
             $user= pg_query($dbconn,$sql);
+            echo $user;
             if(pg_num_rows($user) > 0 )
             {  
                $row_data= pg_fetch_array($user);

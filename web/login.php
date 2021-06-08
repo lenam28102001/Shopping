2d3ec2db5344abe867f89b544fdac72c9ded9c6d
Le Nam <namlgcd191254@fpt.edu.vn>
2021-06-08 14:45:41 +0700

    354442

--- web/login.php
+++ web/login.php
@@ -1,35 +1,6 @@
 <?php
       session_start();
-      require_once "config.php";
-      if(isset($_POST['submit']) && $_POST["username"] != '' && $_POST["password"] != '')
-      {
-         //thực hiện xử lý khi người dùng ấn submit và điền đầy đủ thông tin
-         $username = $_POST["username"];
-         $password = $_POST["password"];
-         $password = md5($password);
-         $sql = "select * from public.user where username = '".$username."' and password = '".$password."'";
-         $user= pg_query($dbconn,$sql);
-         echo $user;
-         if(pg_num_rows($user) > 0 )
-         {  
-            $row_data= pg_fetch_array($user);
-            $_SESSION['user']= $username;
-            $_SESSION['id_khachhang']= $row_data['id_user'];
-            header("location:../index.php");
-         }
-         else
-         {
-            $_SESSION["thongbao"] = "Wrong username or password!";
-            header("location: ./login.php");
-         }
-      } 
-      else
-      {
-         $_SESSION["thongbao"]="Please enter your complete information!";
-         header("location:./login.php");
-      }
-      if(isset($_POST["reset"]))
-      header("location: ./register.php");
+      
 ?>
 <!DOCTYPE html>
 <html>
@@ -49,7 +20,7 @@
                   }
                ?>
          </div>
-         <form  method ="POST" >
+         <form  action = "./login_submit.php" method ="POST" >
             <!-- Vertical -->
             <div class="form-group">
                <label for="myEmail">User</label>

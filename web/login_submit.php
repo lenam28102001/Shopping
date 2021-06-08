9f928c39401cf732e7e47767c7722eafbcbf8c7c
Le Nam <namlgcd191254@fpt.edu.vn>
2021-06-08 14:09:15 +0700

    32

--- web/login_submit.php
+++ web/login_submit.php
@@ -1,16 +1,15 @@
 <?php
     session_start();
     require_once "config.php";
-    $dbconn = pg_connect($conn_string);
          if(isset($_POST['submit']) && $_POST["username"] != '' && $_POST["password"] != '')
          {
             //thực hiện xử lý khi người dùng ấn submit và điền đầy đủ thông tin
             $username = $_POST["username"];
             $password = $_POST["password"];
             $password = md5($password);
-            $sql = "select * from public.user where username='$username' and password = '$password' limit 1";
+            $sql = "select * from public.user where public.username ='$username' and public.password = '$password' limit 1";
             $user= pg_query($dbconn,$sql);
-            if(pg_num_rows($user) >0 )
+            if(pg_num_rows($user) > 0 )
             {  
                $row_data= pg_fetch_array($user);
                $_SESSION['user']= $username;
